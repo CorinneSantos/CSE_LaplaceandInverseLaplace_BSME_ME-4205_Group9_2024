97b491465ec2302c24954dd09d5d34f1ad4b17bb
Noemie Noche <159034322+nuymenutsi@users.noreply.github.com>
2024-02-28 20:40:59 +0800

    Add files via upload

---
+++ Part_III.m
@@ -0,0 +1,22 @@
+%Clear
+clear
+clc
+close all
+
+syms t s
+
+%% III. Inverse Laplace Transform
+%% Problem 1
+F7 = 1 / ((s) * (s^2 + 2*s + 2));
+f7 = ilaplace(F7);
+pretty(f7)
+
+%% Problem 2
+F8 = ((5) * (s + 2)) / ((s^2) * (s + 1) * (s + 3));
+f8 = ilaplace(F8);
+pretty(f8)
+
+%% Problem 3
+F9 = (s^4 + 2*s^3 + 3*s^2 + 4*s + 5) / ((s) * (s + 1));
+f9 = ilaplace(F9);
+pretty(f9)
